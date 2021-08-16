b03a9137d620b9993f4fac0a83d402185f0d56b5
Himani Trivedi <himanitrivedi1874@gmail.com>
2021-08-16 13:45:54 +0530

    Title Added

--- js/app.js
+++ js/app.js
@@ -1,30 +1,66 @@
 showNotes();
 
 let noteTxt = document.getElementById('addTxt');
+let titleTxt = document.getElementById('titleTxt');
+let alarm=document.getElementById('errorTxt');
+
 document.getElementById('addBtn').addEventListener('click', function () {
     let notes = localStorage.getItem('notes');
+    let titles=localStorage.getItem('title');
+
+    
+    if(titles == null){
+        titleObj=[];
+    }else{
+        titleObj=JSON.parse(titles);
+    }
+
     if (notes == null) {
         noteObj = [];
     } else {
         noteObj = JSON.parse(notes);
     }
-    if(noteTxt.value != ""){
-        noteObj.push(noteTxt.value)
-        localStorage.setItem('notes', JSON.stringify(noteObj));
-        // console.log(noteObj)
-        noteTxt.value = "";
+
+    if(titleTxt.value !=""){
+        if(noteTxt.value != "" ){
+            noteObj.push(noteTxt.value);
+            titleObj.push(titleTxt.value);
+
+            localStorage.setItem('notes', JSON.stringify(noteObj));
+            localStorage.setItem('title', JSON.stringify(titleObj));
+
+            noteTxt.value = "";
+            titleTxt.value = "";
+
+        }else{
+            document.getElementById('error').style.display="block";
+            alarm.innerText="First Write Note!"
+            noteTxt.focus();
+        }
     }else{
-        // console.log("Empty");
         document.getElementById('error').style.display="block";
-        noteTxt.focus();
+        alarm.innerText="Write Title First!"
+        titleTxt.focus();
     }
+    
+    noteTxt.addEventListener('blur',()=>{
+        document.getElementById('error').style.display="none";
+    })
+
+    titleTxt.addEventListener('blur',()=>{
+        document.getElementById('error').style.display="none";
+    })
+    
     showNotes();
  
 });
 
 
+
+
 function showNotes() {
     let noteNode = document.getElementById('notes');
+
     let notes = localStorage.getItem('notes');
     if (notes == null) {
         noteObj = [];
@@ -32,14 +68,24 @@ function showNotes() {
         noteObj = JSON.parse(notes);
     }
 
+    let titles=localStorage.getItem('title');
+    
+    if(titles == null){
+        titleObj=[];
+    }else{
+        titleObj=JSON.parse(titles);
+    }
+
     let html="";
+
     noteObj.forEach(function(element,index){
         let title= new Date();
         let final_title=title.getDate() + "-" + title.getMonth() + "-" + title.getFullYear() + "<br>" + title.getHours() + ":" + title.getMinutes() + ":" + title.getSeconds();
        html+= `    
             <div class="noteCard m-2 card" style="width: 18rem;">
             <div class="card-body">
-                <h5 class="card-title">${final_title}</h5>
+                <h3 class="card-title">${titleObj[index]}</h3>
+                <h6 class="card-title text-danger"> Noted at :${final_title}</h6>
                 <p class="card-text"> ${element}</p>
                 <button id="${index}"onclick="deleteNote(this.id)" class="btn btn-danger">Delete Note</button>
             </div>
@@ -57,15 +103,26 @@ function showNotes() {
 function deleteNote(index){
 
     let notes = localStorage.getItem('notes');
+    let title = localStorage.getItem('title');
+
     if (notes == null) {
         noteObj = [];
     } else {
         noteObj = JSON.parse(notes);
     }
 
+    if (title == null) {
+        titleObj = [];
+    } else {
+        titleObj = JSON.parse(title);
+    }
+
     noteObj.splice(index,1);
+    titleObj.splice(index,1);
 
     localStorage.setItem('notes',JSON.stringify(noteObj));
+    localStorage.setItem('title',JSON.stringify(titleObj));
+
     showNotes();
 
 }
